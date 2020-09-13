1341b6610d3e8786fe9a3075f6a65ff23429253c
Shalpreet Kaur <kaurshalpreet@gmail.com>
2020-09-12 18:03:23 -0700

    Added margin to the employee cards

--- app.js
+++ app.js
@@ -18,76 +18,75 @@ addMember();
 // code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 function addMember() {
-    inquirer.prompt([{
-    type: "input",
-    message: "Enter Team Member's name.",
-    name: "name"
-},
-{
-    type: "list",
-    message: "Select Team Member's Role.",
-    choices: [
-        "Engineer",
-        "Intern",
-        "Manager"
-    ],
-    name: "role"
-},
-{
-    type: "input",
-    message: "Enter Team Member's Id.",
-    name: "id"
-},
-{
-    type: "input",
-    message: "Enter Team Member's email address.",
-    name: "email"
-}]).then(function( {name, role, id, email} ) {
-    let roleInfo = "";
-    if ( role === "Engineer"){
-        roleInfo = "GitHub username";
-    } else if ( role === "Intern") {
-        roleInfo = "School Name";
-    } else {
-        roleInfo = "Office Phone Number"
-    }
-    inquirer.prompt([{
-        message: `Enter Team Member's ${roleInfo}`,
-        name: "roleInfo"
-    },
-    {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter Team Member's name.",
+        name: "name",
+      },
+      {
         type: "list",
-        message: "Would you like to add more Team Members?",
-        choices: [
-            "Yes",
-            "No"
-        ],
-        name: "addMembers"
-    }]).then(function( {roleInfo, addMembers} ) {
-        let newMember;
-        if (role === "Engineer") {
+        message: "Select Team Member's Role.",
+        choices: ["Engineer", "Intern", "Manager"],
+        name: "role",
+      },
+      {
+        type: "input",
+        message: "Enter Team Member's Id.",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "Enter Team Member's email address.",
+        name: "email",
+      },
+    ])
+    .then(function ({ name, role, id, email }) {
+      let roleInfo = "";
+      if (role === "Engineer") {
+        roleInfo = "GitHub username";
+      } else if (role === "Intern") {
+        roleInfo = "School Name";
+      } else {
+        roleInfo = "Office Phone Number";
+      }
+      inquirer
+        .prompt([
+          {
+            message: `Enter Team Member's ${roleInfo}`,
+            name: "roleInfo",
+          },
+          {
+            type: "list",
+            message: "Would you like to add more Team Members?",
+            choices: ["Yes", "No"],
+            name: "addMembers",
+          },
+        ])
+        .then(function ({ roleInfo, addMembers }) {
+          let newMember;
+          if (role === "Engineer") {
             newMember = new Engineer(name, id, email, roleInfo);
-        } else if (role === "Intern") {
-            newMember = new Intern (name, id, email, roleInfo);
-        } else {
-            newMember = new Manager (name, id, email, roleInfo);
-        }
-        employees.push(newMember);
-        console.log(employees);
+          } else if (role === "Intern") {
+            newMember = new Intern(name, id, email, roleInfo);
+          } else {
+            newMember = new Manager(name, id, email, roleInfo);
+          }
+          employees.push(newMember);
+          console.log(employees);
 
-        if (addMembers === "Yes") {
-                    addMember();
-                } else {
-                 const html = render(employees);
-                    fs.writeFile("./output/team.html", html, function (err) {
-                                    if (err) {
-                                        console.log(err);
-                                    };
-                                });
-                    console.log("end");
-                }
-
-    })
-})
+          if (addMembers === "Yes") {
+            addMember();
+          } else {
+            const html = render(employees);
+            fs.writeFile("./output/team.html", html, function (err) {
+              if (err) {
+                console.log(err);
+              }
+            });
+            console.log("end");
+          }
+        });
+    });
 }
-
